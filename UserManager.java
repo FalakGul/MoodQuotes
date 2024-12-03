08328bf36988c40d8338be038b74669de613e725
shibrakhan430 <shibrakhan430@gmail.com>
2024-12-03 12:53:36 +0500

    Create UserManager.java

---
+++ UserManager.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserManager {
+    private Map<String, User> users;
+
+    public UserManager() {
+        users = new HashMap<>();
+    }
+
+    public boolean registerUser(String username, String password) {
+        if (users.containsKey(username)) {
+            return false; // Username already exists
+        }
+        users.put(username, new User(username, password));
+        return true;
+    }
+
+    public User loginUser(String username, String password) {
+        User user = users.get(username);
+        if (user != null && user.getPassword().equals(password)) {
+            return user;
+        }
+        return null; // Invalid login credentials
+    }
+}
